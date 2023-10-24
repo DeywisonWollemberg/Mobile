906676c32007efba68ce3b2c1dbb54828214ef5f
DeywisonWollemberg <130513088+DeywisonWollemberg@users.noreply.github.com>
2023-10-23 21:38:42 -0300

    Alteracao_23_10

--- index.tsx
+++ index.tsx
@@ -1,82 +1,127 @@
 import React from "react";
-import { Button, StyleSheet, Text, TextInput, Touchable, TouchableOpacity, View } from "react-native";
+import { FlatList, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
+
+
+interface Tarefa {
+        id: string;
+        title: string;
+    
+    }
+    
 
 export const Home = () => {
+    
+
+    const [newTask, setNewTask] = React.useState('');
+    const [tarefas, setTarefas] = React.useState<Tarefa[]>([]);
+
+    const adcTarefa = () => {
+        const dados = {
+          id: String(new Date().getTime()),
+          title: newTask ? newTask : "Registro vazio",
+  
+        };
+        setTarefas([...tarefas,dados]);
+        setNewTask('');
+     };
+
+     const removerTarefa = (id: string) => {
+        const tarefasAtualizadas = tarefas.filter((tarefa)=> tarefa.id !== id);
+        setTarefas(tarefasAtualizadas);
+
+     }
+    
 
     return (
         <View style={styles.container}>
+            <Text style={styles.title}>Tarefas do dia</Text>
+            <TextInput 
+            value={newTask}
+            onChangeText={setNewTask} 
+            placeholderTextColor='#555' 
+            placeholder="Tarefas"
+             style={styles.fields}
+              />
+             
 
-            <Text style={styles.logo}>MAACE</Text>
+            <TouchableOpacity activeOpacity={0.7} onPress={adcTarefa} style={styles.button}>
+                
+                <Text  style={styles.buttonText} >Entrar</Text>
 
-            <Text style={styles.title} >Email</Text>
-            
-            <TextInput style={styles.caixa}>  </TextInput>
-
-            <Text style={styles.title} >Senha teste</Text>
-            
-            <TextInput style={styles.caixa} secureTextEntry={true}>  </TextInput>
-
-            <TouchableOpacity style={styles.botao}> 
-                <Text style={styles.texto}> Login </Text>
             </TouchableOpacity>
             
-        </View>
+            <Text style={styles.title}>Lista de tarefas</Text>
 
+           {/* <ScrollView >
+
+                {tarefas.map((tarefa : Tarefa) => (
+                        <TouchableOpacity key={tarefa.id}>
+                            <Text style={styles.buttonText}>{tarefa.title}</Text>
+                        </TouchableOpacity>
+                ))}
+
+            </ScrollView> */}
+            <FlatList
+                data={tarefas}
+                keyExtractor={(item: Tarefa) => item.id}
+                renderItem={({item}: {item: Tarefa}) =>  (
+                <TouchableOpacity style={styles.renderList}
+                onPress={() => removerTarefa(item.id)}
+                >
+                    <Text style={styles.buttonText} >{item.title}</Text>
+                </TouchableOpacity>
+                  )}>
+
+
+            </FlatList>
         
-
-
-
+        </View>
     );
 };
 
 const styles = StyleSheet.create({
-
-    logo: {
-        color: 'white',
-        fontWeight: 'bold',
-        fontSize: 60,
-        marginBottom: 80,
-    },
-
     container: {
-        flex: 1,
-        backgroundColor: '#121214',
-        alignItems: 'center',
-        justifyContent: 'center',
-    }, 
-    
+      flex: 1,
+      backgroundColor: '#121214',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
     title: {
         color: '#f1f1f1',
         fontWeight: 'bold',
-        fontsize: 15,
+        fontSize: 15,
+        paddingTop: 15,
     },
-
-    caixa:{
-        backgroundColor:'#f1f1f1',
-        width: 200,
-        borderRadius: 20,
-        margin: 10,
-        padding: 5,
-    },
-
-
-    botao:{
-        backgroundColor:'grey',
-        alignItems: 'center',
-        width: 100,
-        borderRadius: 20,
-        margin: 10,
-        padding: 5,
-        
-    },
-
-    texto: {
+    fields: {
+        backgroundColor: 'red',
         color: '#f1f1f1',
-        fontWeight: 'bold',
-        fontsize: 15,
+        fontSize: 18,
+        padding: 15,
+        marginTop: 30,
+        borderRadius: 7,
+        width: 300,
+        alignItems: 'center',
+        textAlign: 'center',
     },
-    
+    button: {
+        backgroundColor: 'rgb(192, 192, 193)',
+        padding: 15,
+        borderRadius: 7,
+        alignItems: 'center',
+        marginTop: 20,
+        width: 300,
+    },
+    buttonText: {
+        color: '#fff',
+        fontSize: 15,
+    },
 
-
-    
-});
+    renderList: {
+        backgroundColor: 'red',
+        padding:10,
+        marginTop:10,
+        borderRadius:6,
+        alignItems:'center',
+    },
+  });
+  
